631a7017cd8c9b17d6fe3a28b55d725d42f79b89
okkindel <ebrithil97@gmail.com>
2019-10-15 20:46:14 +0200

    feat(points): it looks that it kinda works

--- src/demo/square.demo.ts
+++ src/demo/square.demo.ts
@@ -1,4 +1,9 @@
-import { HypPolygon, Point, CartesianPoint } from "../core/entity";
+import {
+  HypPolygon,
+  Point,
+  CartesianPoint,
+  HyperbolicPoint
+} from "../core/entity";
 import { Canvas } from "../core/canvas";
 import { Plane } from "../core/plane";
 
@@ -11,14 +16,64 @@ export class SquaewDemo {
     this.plane = Plane.getInstance();
     this.canvas = canvas;
 
-    const point1 = new CartesianPoint(-0.5, 0.5, this.plane).toCanvasCoords();
-    const point2 = new CartesianPoint(0.5, 0.5, this.plane).toCanvasCoords();
-    const point3 = new CartesianPoint(0.5, -0.5, this.plane).toCanvasCoords();
-    const point4 = new CartesianPoint(-0.5, -0.5, this.plane).toCanvasCoords();
+    const point1 = new HyperbolicPoint(
+      new CartesianPoint(-0.5, 0.5, this.plane),
+      this.plane
+    ).toCanvasCoords();
+    const point2 = new HyperbolicPoint(
+      new CartesianPoint(0.5, 0.5, this.plane),
+      this.plane
+    ).toCanvasCoords();
+    const point3 = new HyperbolicPoint(
+      new CartesianPoint(0.5, -0.5, this.plane),
+      this.plane
+    ).toCanvasCoords();
+    const point4 = new HyperbolicPoint(
+      new CartesianPoint(-0.5, -0.5, this.plane),
+      this.plane
+    ).toCanvasCoords();
 
     this.polygon = new HypPolygon(point1, point2, this.plane);
     this.polygon.addVerticle(point3);
     this.polygon.addVerticle(point4);
+
+    window.addEventListener("mousemove", e => {
+      console.log(
+        new Point(
+          e.clientX,
+          canvas.canvas.height - e.clientY
+        ).toCartesianCoords(this.plane)
+      );
+
+      const points = new Point(
+        e.clientX,
+        canvas.canvas.height - e.clientY
+      ).toCartesianCoords(this.plane);
+
+      const x = points.x;
+      const y = points.y;
+
+      const point1 = new HyperbolicPoint(
+        new CartesianPoint(x - 0.3, y + 0.3, this.plane),
+        this.plane
+      ).toCanvasCoords();
+      const point2 = new HyperbolicPoint(
+        new CartesianPoint(x + 0.3, y + 0.3, this.plane),
+        this.plane
+      ).toCanvasCoords();
+      const point3 = new HyperbolicPoint(
+        new CartesianPoint(x + 0.3, y - 0.3, this.plane),
+        this.plane
+      ).toCanvasCoords();
+      const point4 = new HyperbolicPoint(
+        new CartesianPoint(x - 0.3, y - 0.3, this.plane),
+        this.plane
+      ).toCanvasCoords();
+
+      this.polygon = new HypPolygon(point1, point2, this.plane);
+      this.polygon.addVerticle(point3);
+      this.polygon.addVerticle(point4);
+    });
   }
 
   createLoop() {
@@ -30,6 +85,19 @@ export class SquaewDemo {
       this.polygon.verticles.forEach(verticle => {
         this.canvas.drawPoint(verticle);
       });
+
+      const point1 = new CartesianPoint(-0.5, 0.5, this.plane).toCanvasCoords();
+      const point2 = new CartesianPoint(0.5, 0.5, this.plane).toCanvasCoords();
+      const point3 = new CartesianPoint(0.5, -0.5, this.plane).toCanvasCoords();
+      const point4 = new CartesianPoint(
+        -0.5,
+        -0.5,
+        this.plane
+      ).toCanvasCoords();
+      this.canvas.drawPoint(point1);
+      this.canvas.drawPoint(point2);
+      this.canvas.drawPoint(point3);
+      this.canvas.drawPoint(point4);
 
       this.canvas.setColors("#bbb5");
       this.polygon.getCompletePolygonLines().forEach(element => {
